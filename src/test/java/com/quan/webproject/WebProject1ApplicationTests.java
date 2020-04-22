dc7c32a292cf1625886ffa502a5185f053fdf38b
liujiahao <liujiahao@emsystems-sh.com>
2020-04-22 18:27:38 +0800

    initial

---
+++ src/test/java/com/quan/webproject/WebProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quan.webproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebProject1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
